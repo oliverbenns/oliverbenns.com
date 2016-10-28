857cdc02f7b1b9ef0b1ba142cf5967efae2ee719
Oliver Benns <oliverbenns@gmail.com>
2016-10-28 16:04:36 +0100

    Add basic project page

---
+++ pages/work/project/index.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import Helmet from 'react-helmet'
+import { config } from 'config'
+import Fullscreen from 'components/fullscreen'
+import Panel from 'components/panel'
+import Wrapper from 'components/wrapper'
+
+export default class Project extends Component {
+  render() {
+    return (
+      <div>
+        <Helmet
+          title={`Project | ${config.siteTitle}`}
+          meta={[
+            {"name": "description", "content": "Sample"},
+            {"name": "keywords", "content": "sample, something"},
+          ]}
+        />
+        <Wrapper>
+          <h1>Project</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </Wrapper>
+      </div>
+    )
+  }
+}
