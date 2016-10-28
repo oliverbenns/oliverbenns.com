3719f5d7ea42488f985a5170b384a015297503d9
Oliver Benns <oliverbenns@gmail.com>
2016-10-28 21:23:51 +0100

    Add icon component

---
+++ components/icon/index.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+import styles from './styles.module.css';
+
+export default class Icon extends Component {
+  static propTypes = {
+    name: PropTypes.string,
+  };
+
+  render() {
+    const src = require(`./svg/${this.props.name}.svg`)
+
+    return (
+      <img src={src} className={`${styles.icon} ${this.props.className}`} alt={`${this.props.name} icon`} />
+    );
+  }
+}
