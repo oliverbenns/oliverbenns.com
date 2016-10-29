9216de03d2a49042f021a185c18501b7c746d49e
Oliver Benns <oliverbenns@gmail.com>
2016-10-29 18:31:54 +0100

    Add functionality to close menu on press of esc key

--- components/menu/index.jsx
+++ components/menu/index.jsx
@@ -13,21 +13,30 @@ export default class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = store.data;
+    this.handleKeyPress = this.handleKeyPress.bind(this)
   }
 
   componentDidMount() {
     store.subscribe(this.handleStateChange.bind(this));
+    // @TODO: Remove this event listener when menu isn't open.
+    document.addEventListener('keydown', this.handleKeyPress);
   }
 
   componentWillUnmount() {
     store.unsubscribe(this.handleStateChange.bind(this));
   }
 
+  handleKeyPress(event) {
+    if (event.keyCode === 27) {
+      this.close();
+    }
+  }
+
   handleStateChange() {
     this.setState(store.data);
   }
 
-  handleLinkClick() {
+  close() {
     actions.toggleMenu({ active: false });
   }
 
@@ -38,16 +47,16 @@ export default class Menu extends Component {
           <nav>
             <ul>
               <li className={styles.item}>
-                <Link to={prefixLink('/')} onClick={this.handleLinkClick}>Home</Link>
+                <Link to={prefixLink('/')} onClick={this.close}>Home</Link>
               </li>
               <li className={styles.item}>
-                <Link to={prefixLink('/about/')} onClick={this.handleLinkClick}>About</Link>
+                <Link to={prefixLink('/about/')} onClick={this.close}>About</Link>
               </li>
               <li className={styles.item}>
-                <Link to={prefixLink('/work/')} onClick={this.handleLinkClick}>Work</Link>
+                <Link to={prefixLink('/work/')} onClick={this.close}>Work</Link>
               </li>
               <li className={styles.item}>
-                <Link to={prefixLink('/contact/')} onClick={this.handleLinkClick}>Contact</Link>
+                <Link to={prefixLink('/contact/')} onClick={this.close}>Contact</Link>
               </li>
             </ul>
           </nav>
