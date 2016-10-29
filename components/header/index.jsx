6a91fb6b81041dc13f3e668ec438f3c4599d3f38
Oliver Benns <oliverbenns@gmail.com>
2016-10-29 20:15:38 +0100

    Add new GitHub button

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Hamburger from 'components/hamburger'
 import Menu from 'components/menu'
+import GitHubButton from 'react-github-button'
 import Wrapper from 'components/wrapper'
 
 import actions from 'assets/js/actions';
 import store from 'assets/js/store';
 
 import styles from './styles.module.css'
+import 'react-github-button/assets/style.css';
 
 export default class Header extends Component {
   constructor(props) {
@@ -31,9 +33,7 @@ export default class Header extends Component {
       <header className={styles.header}>
         <Wrapper>
           <div className={styles.inner}>
-            <div className={styles.github}>
-              <a className="github-button" href="https://github.com/oliverbenns/oliverbenns.com" data-icon="octicon-star" data-count-href="/oliverbenns/oliverbenns.com/stargazers" data-count-api="/repos/oliverbenns/oliverbenns.com#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star oliverbenns/oliverbenns.com on GitHub">Star</a>
-            </div>
+            <GitHubButton type="stargazers" size="normal" namespace="oliverbenns" repo="oliverbenns.com" />
             <Hamburger onClick={actions.toggleMenu.bind(this)} active={this.state.menuActive} className={styles.hamburger} />
           </div>
         </Wrapper>
