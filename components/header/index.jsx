37a1bde4675c54ddc6ae4c830c9dc8a5da94f6c2
Oliver Benns <oliverbenns@gmail.com>
2016-10-29 13:52:01 +0100

    Add hamburger to header and adjust layout to fit

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
+import Hamburger from 'components/hamburger'
 import Wrapper from 'components/wrapper'
 
 import styles from './styles.module.css'
@@ -10,7 +11,8 @@ export default class Header extends Component {
     return (
       <header className={styles.header}>
         <Wrapper>
-          <ul className={styles.nav}>
+          <div className={styles.inner}>
+            <ul>
               <li className={styles.item}>
                 <Link to={prefixLink('/')}>Home</Link>
               </li>
@@ -23,9 +25,11 @@ export default class Header extends Component {
               <li className={styles.item}>
                 <Link to={prefixLink('/contact/')}>Contact</Link>
               </li>
-          </ul>
+            </ul>
           <div className={styles.github}>
             <a className="github-button" href="https://github.com/oliverbenns/oliverbenns.com" data-icon="octicon-star" data-count-href="/oliverbenns/oliverbenns.com/stargazers" data-count-api="/repos/oliverbenns/oliverbenns.com#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star oliverbenns/oliverbenns.com on GitHub">Star</a>
+          </div>
+          <Hamburger className={styles.hamburger}/>
           </div>
         </Wrapper>
       </header>
