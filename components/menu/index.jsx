3dea0f5d2f29fe65b3f6b032e537011f6a764fd8
Oliver Benns <oliverbenns@gmail.com>
2016-10-29 20:16:33 +0100

    Add active link class to menu

--- components/menu/index.jsx
+++ components/menu/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router'
+import { IndexLink, Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Fullscreen from 'components/fullscreen'
 import Wrapper from 'components/wrapper'
@@ -41,22 +41,28 @@ export default class Menu extends Component {
   }
 
   render() {
+
+    const linkProps = {
+      onClick: this.close,
+      activeClassName: styles.active
+    };
+
     return (
       <Fullscreen className={this.state.menuActive ? styles.menu : styles.disable}>
         <Wrapper>
           <nav>
             <ul>
               <li className={styles.item}>
-                <Link to={prefixLink('/')} onClick={this.close}>Home</Link>
+                <IndexLink {...linkProps} to={prefixLink('/')}>Home</IndexLink>
               </li>
               <li className={styles.item}>
-                <Link to={prefixLink('/about/')} onClick={this.close}>About</Link>
+                <Link {...linkProps} to={prefixLink('/about/')}>About</Link>
               </li>
               <li className={styles.item}>
-                <Link to={prefixLink('/work/')} onClick={this.close}>Work</Link>
+                <Link {...linkProps} to={prefixLink('/work/')}>Work</Link>
               </li>
               <li className={styles.item}>
-                <Link to={prefixLink('/contact/')} onClick={this.close}>Contact</Link>
+                <Link {...linkProps} to={prefixLink('/contact/')}>Contact</Link>
               </li>
             </ul>
           </nav>
