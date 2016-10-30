aef8fa4ecfdce3b5af5a9e4f23afbc0f8c120bcc
Oliver Benns <oliverbenns@gmail.com>
2016-10-30 18:29:17 +0000

    Use content page component on about

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Helmet from 'react-helmet'
 import { config } from 'config'
-import Fullscreen from 'components/fullscreen'
+import ContentPage from 'components/content-page'
 import Wrapper from 'components/wrapper'
 
 import styles from './styles.module.css'
@@ -11,7 +11,7 @@ import styles from './styles.module.css'
 export default class About extends Component {
   render() {
     return (
-      <div className={styles.about}>
+      <ContentPage>
         <Helmet
           title={`About | ${config.siteTitle}`}
           meta={[
@@ -19,16 +19,14 @@ export default class About extends Component {
             {"name": "keywords", "content": "sample, something"},
           ]}
         />
-        <Fullscreen firstItem>
-          <Wrapper>
-            <div className={styles.intro}>
-              <h1>Hello, world.</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            </div>
-          </Wrapper>
-        </Fullscreen>
-      </div>
+        <Wrapper>
+          <div className={styles.intro}>
+            <h1>Hello world.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          </div>
+        </Wrapper>
+      </ContentPage>
     )
   }
 }
