a7ea6d6988dda71a283dce5d74d5dd4f721e7745
Oliver Benns <oliverbenns@gmail.com>
2016-10-30 17:04:27 +0000

    Add styling to about paage

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -11,7 +11,7 @@ import styles from './styles.module.css'
 export default class About extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.about}>
         <Helmet
           title={`About | ${config.siteTitle}`}
           meta={[
@@ -21,9 +21,11 @@ export default class About extends Component {
         />
         <Fullscreen firstItem>
           <Wrapper>
-            <h1>Hello, world.</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <img src="http://placehold.it/500x400" />
+            <div className={styles.intro}>
+              <h1>Hello, world.</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
           </Wrapper>
         </Fullscreen>
       </div>
