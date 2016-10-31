a1857cf2d5a661917761c038d9e8384e7c69c246
Oliver Benns <oliverbenns@gmail.com>
2016-10-31 18:46:01 +0000

    Add ahm image

--- pages/work/ahm/index.jsx
+++ pages/work/ahm/index.jsx
@@ -4,13 +4,22 @@ import { projects } from 'data/projects.json';
 import Wrapper from 'components/wrapper';
 import { ProjectIntro, ProjectPage } from 'components/project';
 
+import coverImage from 'components/project/img/ahm.png';
+import styles from './styles.module.css';
+
 export default class Ahm extends Component {
   render() {
     const project = projects.find(_project => _project.slug === 'ahm');
 
+    const image = (
+      <div className={styles.image}>
+        <img src={coverImage} />
+      </div>
+    );
+
     return (
       <ProjectPage project={project}>
-        <ProjectIntro project={project} />
+        <ProjectIntro project={project} image={image} />
       </ProjectPage>
     );
   }
