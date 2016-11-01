98022be9b118bcfdb74f1292c4b9046059283918
Oliver Benns <oliverbenns@gmail.com>
2016-11-01 12:53:05 +0000

    Add ahm MacBook carousel

--- pages/work/ahm/index.jsx
+++ pages/work/ahm/index.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import { projects } from 'data/projects.json';
 
+import Macbook from 'components/macbook';
 import Wrapper from 'components/wrapper';
 import { ProjectDescription, ProjectIntro, ProjectPage } from 'components/project';
 
 import coverImage from 'components/project/img/ahm.png';
 import styles from './styles.module.css';
+
+const images = [
+  {
+    src: require('./img/product.jpg'),
+    description: 'Ahm product',
+  },
+  {
+    src: require('./img/products.jpg'),
+    description: 'Ahm product listing',
+  },
+  {
+    src: require('./img/modal.jpg'),
+    description: 'Ahm ambulance services modal',
+  },
+  {
+    src: require('./img/home.jpg'),
+    description: 'Ahm home',
+  },
+  {
+    src: require('./img/manage.jpg'),
+    description: 'Ahm manage health over',
+  },
+  {
+    src: require('./img/extras.jpg'),
+    description: 'Ahm add extras',
+  }
+];
 
 export default class Ahm extends Component {
   render() {
@@ -27,6 +55,9 @@ export default class Ahm extends Component {
           <h3>Role</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
         </ProjectDescription>
+        <Wrapper>
+          <Macbook images={images} />
+        </Wrapper>
       </ProjectPage>
     );
   }
