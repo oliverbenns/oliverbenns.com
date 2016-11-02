547f732b4e088ce9b609891a14ce122fc0224e0a
Oliver Benns <oliverbenns@gmail.com>
2016-11-02 15:17:36 +0000

    Add iphone component

---
+++ components/iphone/index.jsx
@@ -0,0 +1,23 @@
+import React, { Component, PropTypes } from 'react';
+
+import whiteFrameSrc from './img/iphone-white.png';
+import blackFrameSrc from './img/iphone-black.png';
+import styles from './styles.module.css';
+
+export default class Iphone extends Component {
+  static propTypes = {
+    children: PropTypes.array,
+    invert: PropTypes.bool,
+  };
+
+  render() {
+    return (
+      <div className={styles.iphone}>
+        <img src={this.props.invert ? blackFrameSrc : whiteFrameSrc} alt="iPhone Frame" />
+        <div className={styles.content}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
