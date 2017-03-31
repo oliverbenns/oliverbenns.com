ef3819a73524b5136d1625025fcfe3875e946282
Oliver Benns <oliverbenns@gmail.com>
2017-03-31 15:36:00 +0900

    Make meta component a pure function

--- components/meta/index.jsx
+++ components/meta/index.jsx
@@ -1,67 +1,40 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { config } from 'config';
 import Helmet from 'react-helmet';
 
-export default class Meta extends Component {
-  static propTypes = {
-    description: PropTypes.string,
-    noIndex: PropTypes.bool,
-    title: PropTypes.string,
+const Meta = ({ description, noIndex, title }, { location }) => {
+  const mainTitle = `${config.siteTitle} - Freelance Web Developer`;
+  const metaTitle = title ? `${title} | ${mainTitle}` : mainTitle;
+  const metaDescription = description || config.description;
+  const absoluteUrl = `${config.siteUrl}${location.pathname}`;
+
+  const meta = [
+    { name: 'description', content: metaDescription },
+    { name: 'og:title', content: metaTitle },
+    { name: 'og:description', content: metaDescription },
+    { property: 'og:url', content: absoluteUrl },
+    { name: 'twitter:title', content: metaTitle },
+    { name: 'twitter:description', content: metaDescription },
+    { property: 'twitter:url', content: absoluteUrl },
+  ];
+
+  if (noIndex) {
+    meta.push({ name: 'robots', content: 'noindex' });
   }
 
-  static contextTypes = {
-    location: PropTypes.object,
-  }
-
-  render() {
-    const { description, noIndex, title } = this.props;
-    const { location } = this.context;
-
-    const mainTitle = `${config.siteTitle} - Freelance Web Developer`;
-    const metaTitle = title ? `${title} | ${mainTitle}` : mainTitle;
-    const metaDescription = description || config.description;
-    const absoluteUrl = `${config.siteUrl}${location.pathname}`;
-
-    const meta = [
-      {
-        name: 'description',
-        content: metaDescription,
-      },
-      {
-        name: 'og:title',
-        content: metaTitle,
-      },
-      {
-        name: 'og:description',
-        content: metaDescription,
-      },
-      {
-        property: 'og:url',
-        content: absoluteUrl,
-      },
-      {
-        name: 'twitter:title',
-        content: metaTitle,
-      },
-      {
-        name: 'twitter:description',
-        content: metaDescription,
-      },
-      {
-        property: 'twitter:url',
-        content: absoluteUrl,
-      },
-    ];
-
-    if (noIndex) {
-      meta.push({
-        name: 'robots',
-        content: 'noindex',
-      });
-    }
-
-    return (
-      <Helmet title={metaTitle} meta={meta} />
-    );
-  }
+  return (
+    <Helmet title={metaTitle} meta={meta} />
+  );
 }
+
+Meta.propTypes = {
+  description: PropTypes.string,
+  noIndex: PropTypes.bool,
+  title: PropTypes.string,
+};
+
+Meta.contextTypes = {
+  location: PropTypes.object,
+};
+
+export default Meta;
