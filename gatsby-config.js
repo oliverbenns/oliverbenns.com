1ebfcf084e26edb507d1c768a7304fceae116293
Oliver Benns <oliverbenns@gmail.com>
2017-08-14 11:54:51 +0100

    Update config

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Oliver Benns',
+    url: 'http://oliverbenns.com',
+    description: 'Oliver Benns is a freelance web developer working with individuals and agencies across the world.',
+  },
+};
