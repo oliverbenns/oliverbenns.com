f2463270ce038b968edf462abf74875109618668
Oliver Benns <oliverbenns@gmail.com>
2017-08-16 12:13:15 +0100

    Add comment

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -9,6 +9,8 @@ import '../css/typography.css';
 
 const Layout = ({ children, location }) => {
   // Need to pass in location here on build. On development the props come from the individual /pages.
+  // This might be bad: https://github.com/gatsbyjs/gatsby/issues/1781#issuecomment-322475987
+  // Hopefully this gets resolved later (or the Webpack 3 / Gatsby 2.0 release will do some tree shaking)
   const appChildren = process.env.NODE_ENV === 'production' ? children({ location }) : children();
 
   return (
