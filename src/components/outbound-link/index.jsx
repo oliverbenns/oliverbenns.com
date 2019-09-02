2547637ed763e42bdae4d2e62fe394557762a5dc
Oliver Benns <oliverbenns@gmail.com>
2019-09-02 18:51:35 +0100

    Remove old work/dirs

--- src/components/outbound-link/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const OutboundLink = props => (
-  <a {...props} href={props.to} target="_blank" rel="noopener noreferrer">{props.children}</a>
-);
-
-export default OutboundLink;
