72cec23d19f966f6b70b1b1097050665ce1bac3d
Oliver Benns <oliverbenns@gmail.com>
2019-09-09 18:33:49 +0100

    Add static server

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+import "net/http"
+
+const port = 3000
+
+func main() {
+	dir := http.Dir("dist")
+	fileServer := http.FileServer(dir)
+	http.Handle("/", fileServer)
+
+	fmt.Printf("Listening on port %d\n", port)
+
+	address := fmt.Sprintf("localhost:%d", port)
+	http.ListenAndServe(address, nil)
+}
