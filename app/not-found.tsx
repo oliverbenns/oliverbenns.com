a7b8e2d90835d608bb74e160ed04317b1cd0854c
Oliver Benns <oliver@benns.co>
2025-05-05 11:00:28 +0100

    x

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import Link from "next/link";
+export const metadata: Metadata = {
+  title: "Not Found - Oliver Benns",
+  description: "Full stack software engineer",
+};
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col gap-4 w-full sm:w-sm mx-auto">
+        <h1 className="text-xl font-bold text-gray-800 leading-snug tracking-tight">
+          Not Found
+        </h1>
+
+        <p className="text-gray-600">That page doesn't seem to be found.</p>
+      </div>
+    </div>
+  );
+}
