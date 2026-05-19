53b0a8c6bf7c42b1ad129e46e5620c15f4e7fb82
Oliver Benns <oliver@benns.co>
2026-05-19 16:44:29 +0700

    Add to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { url: `${BASE_URL}/about`, lastModified: buildTime },
     { url: `${BASE_URL}/work`, lastModified: buildTime },
     { url: `${BASE_URL}/contact`, lastModified: buildTime },
+    {
+      url: `${BASE_URL}/work/bermuda-commercial-bank`,
+      lastModified: buildTime,
+    },
     { url: `${BASE_URL}/work/hedge-ui`, lastModified: buildTime },
     { url: `${BASE_URL}/work/property-track`, lastModified: buildTime },
     { url: `${BASE_URL}/work/elwood`, lastModified: buildTime },
